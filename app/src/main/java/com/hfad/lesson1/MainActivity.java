dbaaa2f3a0cb4f5ed27c2b8f4ec941148116b841
AnatolyiTsyplenkov <av_tsyplenkov@mail.ru>
2021-08-18 01:32:26 +0300

    ДЗ по уроку 1

--- app/src/main/java/com/hfad/lesson1/MainActivity.java
+++ app/src/main/java/com/hfad/lesson1/MainActivity.java
@@ -2,7 +2,9 @@ package com.hfad.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(view -> {
+            Intent nextScreen = new Intent(getApplicationContext(), SecondActivity.class);
+            startActivity(nextScreen);
+        });
     }
 }
